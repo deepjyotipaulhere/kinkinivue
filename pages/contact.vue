1867f6b103cdcee2c6d47d79a280a00033092f9d
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2020-07-15 12:19:59 +0530

    corrections

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,24 +1,35 @@
 <template>
   <v-app>
     <v-card class="mx-auto mt-10" max-width="344" shaped elevation="15">
+      <v-img
+        class="white--text align-end"
+        height="400px"
+        src="http://kinkiniroy.com:1337/uploads/IMG_9033_min_3_5e03c0b3ec.jpeg"
+      >
+        <v-card-title>Contact</v-card-title>
+      </v-img>
       <v-list-item three-line>
         <v-list-item-content>
-          <div class="overline mb-4">Contact</div>
           <v-list-item-title class="headline mb-1">Kinkini Roy</v-list-item-title>
-          <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+          <v-list-item-subtitle>Paper and Thread artwork.</v-list-item-subtitle>
         </v-list-item-content>
 
         <!-- <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar> -->
       </v-list-item>
 
       <v-card-actions class="white justify-center">
-        <v-btn color="grey" class="white--text" fab icon small>
+        <v-btn color="grey" class="white--text" fab icon small to="mailto:kinkiniroy2012@gmail.com">
           <v-icon color="#D44638">mdi-gmail</v-icon>
         </v-btn>
-        <v-btn color="grey" class="white--text" fab icon small>
-          <v-icon color="#3B5998">mdi-facebook</v-icon>
-        </v-btn>
-        <v-btn color="grey" class="white--text" fab icon small>
+        <v-btn
+          color="grey"
+          class="white--text"
+          fab
+          icon
+          small
+          to="https://www.instagram.com/kaepapercraft/"
+          target="_blank"
+        >
           <v-icon color="#c13584">mdi-instagram</v-icon>
         </v-btn>
       </v-card-actions>
--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,14 @@
     <v-row>
       <v-col cols="12" md="5">
         <v-img src="/kinkiniroylogo.png" width="500"></v-img>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <br>
-        <v-btn to="/quillings" text="" color="primary">Expore Quillings</v-btn>
-        <v-btn to="/earrings" text="" color="success">Expore Earrings</v-btn>
-        <v-btn to="/embroideries" text="" color="warning">Expore Embroideries</v-btn>
+        <br />
+        <p>I am a self-taught artist, starting with water and oil, slowly treading into paper as an art form. When I was painting on paper, to now when I am painting with paper, the journey has been incredible and magnificent. Using paper to draw with, to bring out forms and shapes, becomes quite an interesting path for each of my pieces. Paper is such a good medium to experiment with to create art. I am a paper painter using paper to craft out different art forms which can be collected and admired, along with wearables.</p>
+        <p>In my gallery you will also find embroidery art pieces which used thread to paint over different media.</p>
+        <p>Enjoy and <nuxt-link to="/contact">let me know</nuxt-link> for any enquiries.</p>
+        <br />
+        <v-btn to="/quillings" text color="primary">Expore Quillings</v-btn>
+        <v-btn to="/earrings" text color="success">Expore Earrings</v-btn>
+        <v-btn to="/embroideries" text color="warning">Expore Embroideries</v-btn>
       </v-col>
       <v-col cols="12" md="7" sm="1">
         <v-img src="/front.png" class="hidden-xs-only" />
